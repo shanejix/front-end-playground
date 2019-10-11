78482d08a553a52e4ef2e3f392a2a4a0d1fff5c7
shanejix <shanejix@vip.163.com>
2019-10-11 15:54:36 +0800

    add react memo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 // import Comment from './ReactComp/UICompAndContainerComp/Comment'
-import Count from './ReactComp/PureComponent/Count'
+// import Count from './ReactComp/PureComponent/Count'
+import Count from './ReactComp/Memo/Count'
 
 function App() {
   return (
---
+++ src/ReactComp/Memo/Count/index.js
@@ -0,0 +1,5 @@
+
+
+import CountList from './CountList'
+
+export default CountList
