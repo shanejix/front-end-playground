7767f715cc431f38369978bb759c3bbcba66642b
shanejix <shanejix@vip.163.com>
2019-10-16 18:27:56 +0800

    add layout

---
+++ precache-manifest.52bbacd34259fe2a26ec69ad33a445f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac410aa177d56caf9b7b94a4327d923b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7e485002117d4550569",
+    "url": "/static/css/2.ba6f5eba.chunk.css"
+  },
+  {
+    "revision": "4815b2507f55b22f83cd",
+    "url": "/static/css/main.58b75e24.chunk.css"
+  },
+  {
+    "revision": "a7e485002117d4550569",
+    "url": "/static/js/2.fc59bbcf.chunk.js"
+  },
+  {
+    "revision": "4815b2507f55b22f83cd",
+    "url": "/static/js/main.b6a3a351.chunk.js"
+  },
+  {
+    "revision": "0696d524d554410585eb",
+    "url": "/static/js/runtime-main.685e718f.js"
+  }
+]);
