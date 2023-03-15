535c72aa9e5df6c95a2d8c7e9d6030e1144bab35
shanejix <shanejix@hotmail.com>
2023-03-15 22:48:31 +0800

    chore: update struct pattern

---
+++ pattern/adapter.ts
@@ -0,0 +1,25 @@
+class Target {
+  public request(): string {
+      return 'Target: The default target\'s behavior.';
+  }
+}
+
+class Adaptee {
+  public specificRequest(): string {
+      return '.eetpadA eht fo roivaheb laicepS';
+  }
+}
+
+class Adapter extends Target {
+  private adaptee: Adaptee;
+
+  constructor(adaptee: Adaptee) {
+      super();
+      this.adaptee = adaptee;
+  }
+
+  public request(): string {
+      const result = this.adaptee.specificRequest().split('').reverse().join('');
+      return `Adapter: (TRANSLATED) ${result}`;
+  }
+}
